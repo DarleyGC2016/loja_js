8bafd167d7fa04afb409e44f3378c30b99b18257
DarleyGC2016 <garciacardoso.darley@gmail.com>
2025-02-21 18:10:48 -0300

    useState

--- src/components/FomularioCadastro/FomularioCadastro.jsx
+++ src/components/FomularioCadastro/FomularioCadastro.jsx
@@ -1,19 +1,25 @@
 "use client"
 import { Button, TextField, Switch, FormControlLabel } from '@mui/material';
+import { useState } from 'react';
 
 function FomularioCadastro() {
-    let nome = "";
+    const [nome, setNome] = useState("Darley");
     return (
-        <form onSubmit={event => { 
-             event.preventDefault() // preventDefault() - serve para não recarregar a página.
-            console.log(nome) } }>
+        <form onSubmit={event => {
+            event.preventDefault() // preventDefault() - serve para não recarregar a página.
+            console.log(nome)
+        }}>
             <TextField
                 id="nome"
                 label="Nome"
                 variant="outlined"
                 margin='normal'
-                onChange={ event => {
-                    nome = event.target.value;
+                value={nome}
+                onChange={event => {
+                    setNome(event.target.value);
+                    if (nome.length >= 3) {
+                        setNome(nome.substring(0, 3));
+                    }
                 }}
                 fullWidth />
 

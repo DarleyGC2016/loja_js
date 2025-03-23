6e36e7e6999d3a4d41883413f51c4789a9b8855f
DarleyGC2016 <garciacardoso.darley@gmail.com>
2025-03-23 02:23:25 -0300

    task 01 - restruração do projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,21 @@
-"use client"
-import FomularioCadastro from "@/components/FomularioCadastro/FomularioCadastro";
-import { Component } from "react"
-import { Container, Typography } from '@mui/material';
+"use client";
+import { Component } from "react";
+import { Container, Typography } from "@mui/material";
 
-import '@fontsource/roboto/300.css';
-import { IUsuario, IValidacao } from "@/model/model";
-
+import FormUsuario from "@/pages/Usuario/Cadastro/FormUsuario";
+import "@fontsource/roboto/300.css";
 
 class App extends Component {
   render() {
-
     return (
-      <Container component='article' maxWidth='sm'>
-        <Typography variant="h3" align="center" component='h1'>Cadastro da Loja</Typography>
-        <FomularioCadastro
-          aoEnviar={aoEnviarForm}
-          validarCPF={validaCpf}
-          validarNome={validaNome}
-          validarSobrenome={validaSobrenome} />
+      <Container component="article" maxWidth="sm">
+        <Typography variant="h3" align="center" component="h1">
+          Cadastro da Loja
+        </Typography>
+        <FormUsuario />
       </Container>
     );
   }
 }
 
-const aoEnviarForm = (dados: IUsuario): void => {
-  console.log(dados);
-
-}
-
-function validaCpf(cpf: string): IValidacao {
-  if (cpf.length != 11) {
-    return { valido: false, texto: "CPF deve conter 11 digitos!" }
-  } else {
-    return { valido: true, texto: "" }
-  }
-}
-
-const validaNome = (nome: string): IValidacao => {
-  if (nome.length >= 13) {
-    return { valido: false, texto: "Nome deve conter 12 caracter!" }
-  } else {
-    return { valido: true, texto: "" }
-  }
-}
-
-const validaSobrenome = (sobreNome: string): IValidacao => {
-  if (sobreNome.length < 14 || sobreNome.length > 50) {
-    return { valido: false, texto: "Sobrenome deve conter 18 caracter!" }
-  } else {
-    return { valido: true, texto: "" }
-  }
-}
-
-export default App
+export default App;
--- src/model/model.ts
+++ src/model/model.ts
@@ -1,18 +1,25 @@
 export interface IUsuario {
-    nome: string,
-    sobrenome: string,
-    cpf: string,
-    promocoes: boolean,
-    novidades: boolean
+  nome: string;
+  sobrenome: string;
+  cpf: string;
+  promocoes: boolean;
+  novidades: boolean;
 }
 
 export interface IErros {
-    nome: { valido: boolean, texto: string },
-    sobrenome: { valido: boolean, texto: string },
-    cpf: { valido: boolean, texto: string }
+  nome: { valido: boolean; texto: string };
+  sobrenome: { valido: boolean; texto: string };
+  cpf: { valido: boolean; texto: string };
 }
 
 export interface IValidacao {
-    valido: boolean,
-    texto: string
+  valido: boolean;
+  texto: string;
+}
+
+export interface IFunctions {
+  aoEnviar: (dados: IUsuario) => void;
+  validarCPF: (cpf: string) => IValidacao;
+  validarNome: (nome: string) => IValidacao;
+  validarSobrenome: (sobreNome: string) => IValidacao;
 }

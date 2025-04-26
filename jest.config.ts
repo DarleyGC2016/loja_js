1d63a24f24ac81a2f28b36766a5425eb5dd04e66
DarleyGC2016 <garciacardoso.darley@gmail.com>
2025-04-26 00:42:46 -0300

    tak 03 - config jest

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,13 @@
  * https://jestjs.io/docs/configuration
  */
 
+
 import type {Config} from 'jest';
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
 
 const config: Config = {
   // All imported modules in your tests should be mocked automatically
@@ -102,7 +108,7 @@ const config: Config = {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  // preset: undefined,
+   preset: 'ts-jest',
 
   // Run tests from one or more projects
   // projects: undefined,
@@ -137,7 +143,7 @@ const config: Config = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
@@ -196,4 +202,4 @@ const config: Config = {
   // watchman: true,
 };
 
-export default config;
+export default createJestConfig(config);
